3b6e047aed9ca3064cdff8e7b160138292306be3
bastuce <bastuce.commit@gmail.com>
2025-06-13 00:13:20 +0200

    connect to dabase for write, lint error on prisma folder.

--- app/database/page.tsx
+++ app/database/page.tsx
@@ -1,18 +1,35 @@
+import { Card, CardDescription, CardTitle } from '@/components/ui/card'
 import { prisma } from '@/lib/prisma'
 import React from 'react'
+import SelectStar from './select-star'
+import { revalidatePath } from 'next/cache'
 
 export default async function page() {
 
-const reviews = await prisma.review.findMany()
+  const reviews = await prisma.review.findMany()
 
+  const changeRating = async (reviewId: string, star: number) => {
+    'use server'
+    await prisma.review.update({
+      where: { id: reviewId },
+      data: { star }
+    })
+
+    revalidatePath('/database')
+  }
     return (<>
-    <div>page</div>
+    <div>The reviewsse</div>
     {reviews.map((review) => (
-      <div key={review.id}>
-        <h2>{review.name}</h2>
+      <Card key={review.id}>
+        <CardTitle>
+          <h2>{review.name}</h2>
+        </CardTitle>
+        <CardDescription>
         <p>{review.review}</p>
         <p>Rating: {review.star}</p>
-      </div>
+          <SelectStar changeRating={changeRating.bind(null,review.id)} star={review.star}/>
+        </CardDescription>
+      </Card>
     ))}
     </>
   )
---
+++ app/database/select-star.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { Star } from 'lucide-react';
+import { resolveViewport } from 'next/dist/lib/metadata/resolve-metadata';
+import React from 'react'
+
+export default function SelectStar(props: {star: number,changeRating: (star: number) => Promise<void>}) {
+  const { star,changeRating } = props;   
+  const [starValue, setStarValue] = React.useState<null | number>(null);
+  return (
+    <div className='flex items-center gap-1'>
+      {[1, 2, 3, 4, 5].map((i) => {
+        const isFilled = i <= (starValue || star);
+        const isUnfilled = i > (starValue || star);
+        return (
+        <span key={i} >
+            <Button asChild className='p-0 h-6 w-6 rounded-full bg-transparent hover:bg-transparent focus:bg-transparent'
+            onClick={() => changeRating(i)}
+            
+            onMouseEnter={() => {
+                // Handle mouse enter event
+                console.log(`Mouse entered on star ${i}`);
+                setStarValue(i)
+            }}
+            onMouseLeave={() => {
+                // Handle mouse leave event
+                console.log(`Mouse left star ${i}`);
+                setStarValue(null)
+            }}
+            >
+            <Star 
+        className={cn(
+           {'fill-yellow-500 text-yellow-500': isFilled || starValue === i,
+            'fill-transparent text-yellow-500': isUnfilled && starValue !== i},
+        )
+    }/>
+
+            </Button>
+        </span>
+      )}
+      )}
+    </div>
+  )
+}
