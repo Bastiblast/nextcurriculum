93fd3098519be910ad10b389c1858fb39f717a86
bastuce <bastuce.commit@gmail.com>
2025-07-20 01:24:28 +0200

    Merging sql password, modify nodemailer params

--- app/auth/serveVerificationMail.tsx
+++ app/auth/serveVerificationMail.tsx
@@ -1,25 +1,31 @@
 'use server'
 
+import { getSession } from "@/lib/auth-server";
+
 export default async function serveVerificationMail() {
+    const session = await getSession()
 
 const nodemailer = require("nodemailer");
 
 // Create a test account or replace with real credentials.
 const transporter = nodemailer.createTransport({
-host: process.env.NODEMAILER_SERVER,
+host: "localhost",
 port: 25,
 secure: false, // true for 465, false for other ports
 auth: {
-user: process.env.SCW_DEFAULT_PROJECT_ID,
-pass: process.env.SCW_SECRET_KEY,
+user: process.env.POSTFIX_USER,
+pass: process.env.PASSWORD,
 },
+    tls: {
+        rejectUnauthorized: false
+    }
 });
 
 console.log("send mail with transporter",transporter)
 // Wrap in an async IIFE so we can use await.
 const info = await transporter.sendMail({
-    from: '"LionSide Mail Service" <contact@mail.lionside.fr>',
-    to: "test-7z57srsvs@srv1.mail-tester.com",
+    from: '"LionSide Mail Service" <contact@lionside.fr>',
+    to: session?.user.email,
     subject: "Hello ✔",
     text: "Hello world?", // plain‑text body
     html: "<b>Hello world?</b>", // HTML body
